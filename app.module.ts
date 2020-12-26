9e80a814bed6fa4f844ed4bff85be87b52aef1d6
Lucas Marco <54378079+lucas-hub@users.noreply.github.com>
2020-12-26 00:48:28 -0300

    App component

---
+++ app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PokemonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    NgxPaginationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
